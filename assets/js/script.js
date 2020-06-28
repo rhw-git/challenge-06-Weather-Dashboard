3dc37b6ab35f18094a0438485580255f8de58489
rhw <ruohanw89@gmail.com>
2020-06-28 18:46:57 +1000

    fix alert founction for inviliad city name input

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,43 +2,62 @@
 var citiesListArr = [];
 var numOfCities = 9;
 var personalAPIKey = "appid=264dbcaa3899de05eeadc78d68ba06dc";
-
+var unit = "units=imperial";
 var dailyWeatherApiStarts =
   "https://api.openweathermap.org/data/2.5/weather?q=";
-var dailyUVIndexApiStarts = "http://api.openweathermap.org/data/2.5/uvi?";
+var dailyUVIndexApiStarts = "https://api.openweathermap.org/data/2.5/uvi?";
+var forecastWeatherApiStarts =
+  "https://api.openweathermap.org/data/2.5/onecall?";
 // select from html element
 var searchCityForm = $("#searchCityForm");
 var searchedCities = $("#searchedCityLi");
 //-------------------------- get weather info from OpenWeather starts here ------------------------------//
 var getCityWeather = function (searchCityName) {
   // formate the OpenWeather api url
-  var apiUrl = dailyWeatherApiStarts + searchCityName + "&" + personalAPIKey;
+  var apiUrl =
+    dailyWeatherApiStarts + searchCityName + "&" + personalAPIKey + "&" + unit;
   // make a request to url
-  fetch(apiUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      $("#cityName").html(response.name);
-      // display weather icon
-      weatherIncoUrl =
-        "http://openweathermap.org/img/wn/" +
-        response.weather[0].icon +
-        "@2x.png";
-      $("#weatherIconToday").attr("src", weatherIncoUrl);
-      $("#tempToday").html(response.main.temp + " \u00B0F");
-      $("#humidityToday").html(response.main.humidity + " %");
-      $("#windSpeedToday").html(response.wind.speed + " MPH");
-      // return coordinate for getUVIndex to call
-      var lat = response.coord.lat;
-      var lon = response.coord.lon;
-      getUVIndex(lat, lon);
-    });
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      return response.json().then(function (response) {
+        $("#cityName").html(response.name);
+        // display date
+        var unixTime = response.dt;
+        var date = moment.unix(unixTime).format("MM/DD/YY");
+        $("#currentdate").html(date);
+        // display weather icon
+        var weatherIncoUrl =
+          "http://openweathermap.org/img/wn/" +
+          response.weather[0].icon +
+          "@2x.png";
+        $("#weatherIconToday").attr("src", weatherIncoUrl);
+        $("#tempToday").html(response.main.temp + " \u00B0F");
+        $("#humidityToday").html(response.main.humidity + " %");
+        $("#windSpeedToday").html(response.wind.speed + " MPH");
+        // return coordinate for getUVIndex to call
+        var lat = response.coord.lat;
+        var lon = response.coord.lon;
+        getUVIndex(lat, lon);
+        getForecast(lat, lon);
+        createCityNameBtn(searchCityName);
+      });
+    } else {
+      alert("Please provide a valid city name.");
+    }
+  });
 };
+
 var getUVIndex = function (lat, lon) {
   // formate the OpenWeather api url
   var apiUrl =
-    dailyUVIndexApiStarts + personalAPIKey + "&lat=" + lat + "&lon=" + lon;
+    dailyUVIndexApiStarts +
+    personalAPIKey +
+    "&lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&" +
+    unit;
   fetch(apiUrl)
     .then(function (response) {
       return response.json();
@@ -54,7 +73,44 @@ var getUVIndex = function (lat, lon) {
       }
     });
 };
-var getForecast = function (searchCityName) {};
+var getForecast = function (lat, lon) {
+  // formate the OpenWeather api url
+  var apiUrl =
+    forecastWeatherApiStarts +
+    "lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&exclude=current,minutely,hourly" +
+    "&" +
+    personalAPIKey +
+    "&" +
+    unit;
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      for (var i = 1; i < 6; i++) {
+        //display date
+        var unixTime = response.daily[i].dt;
+        var date = moment.unix(unixTime).format("MM/DD/YY");
+        $("#Date" + i).html(date);
+        // display weather icon
+        var weatherIncoUrl =
+          "http://openweathermap.org/img/wn/" +
+          response.daily[i].weather[0].icon +
+          "@2x.png";
+        $("#weatherIconDay" + i).attr("src", weatherIncoUrl);
+        // display temperature
+        var temp = response.daily[i].temp.day + " \u00B0F";
+        $("#tempDay" + i).html(temp);
+        // display humidity
+        var humidity = response.daily[i].humidity;
+        $("#humidityDay" + i).html(humidity + " %");
+      }
+    });
+};
 //-------------------------- get weather info from OpenWeather ends here ------------------------------//
 //-------------------------------------- create button starts  ----------------------------------------//
 var creatBtn = function (btnText) {
@@ -73,7 +129,7 @@ var loadSavedCity = function () {
   }
   for (var i = 0; i < citiesListArr.length; i++) {
     var cityNameBtn = creatBtn(citiesListArr[i]);
-    searchedCities.prepend(cityNameBtn);
+    searchedCities.append(cityNameBtn);
   }
 };
 //---------------------- load saved citeis names from localStorage ends here ------------------------//
@@ -114,21 +170,15 @@ var formSubmitHandler = function (event) {
   event.preventDefault();
   // name of the city
   var searchCityName = $("#searchCity").val().trim();
-  if (searchCityName) {
-    saveCityName(searchCityName);
-    createCityNameBtn(searchCityName);
-    getCityWeather(searchCityName);
-    getForecast(searchCityName);
-  } else {
-    alert("please enter a city name!");
-  }
+  saveCityName(searchCityName);
+
+  getCityWeather(searchCityName);
 };
 var BtnClickHandler = function (event) {
   event.preventDefault();
   // name of the city
   var searchCityName = event.target.textContent.trim();
   getCityWeather(searchCityName);
-  getForecast(searchCityName);
 };
 //--------------------------- event handler from submit form ends here ------------------------------//
 //------------------------ call functions with submit button starts here ----------------------------//
